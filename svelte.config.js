aa44df9c51b8d60fc15e760055a91cbdc9751a86
Aleksey Dolgikh <a@nku.su>
2023-02-25 11:51:28 +0300

    Начало переноса на TypeScript

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-export default {
-    kit: {
-        adapter: node({
-            entryPoint: 'src/server.js'
-        })
-    }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
+
+export default config;
